b0601b2ddcde2309c06a7b30f4b7d8794b4c7efb
James Coliz <jcoliz@outlook.com>
2023-01-18 10:54:07 -0800

    build(arm): Remove hard-coded resource group name

--- .azure/deploy/BringUp.ps1
+++ .azure/deploy/BringUp.ps1
@@ -1,3 +1,8 @@
-$env:RESOURCEGROUP = "azfunc"
+if (-not (Test-Path env:RESOURCEGROUP)) 
+{ 
+    Write-Output "Please set env:RESOURCEGROUP to name of resource group to create and deploy into"
+    Exit 
+}
+
 az group create --name $env:RESOURCEGROUP --location "West US 2"
 az deployment group create --name "Func-$(Get-Random)" --resource-group $env:RESOURCEGROUP --template-file "azuredeploy.json"
